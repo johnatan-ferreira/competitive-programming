d9940fa38d31b3e8e648034a7491a58eb482432b
Johnatan Ferreira <johnatan.fs@protonmail.com>
2026-08-23 17:13:37 -0300

    Correção do 1278

--- 3 - Strings/1278.cpp
+++ 3 - Strings/1278.cpp
@@ -1,60 +1,55 @@
-#include <algorithm>
 #include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
-string corretor(string s[]) {
-    // Remove múltiplos espaços internos deixando apenas um
-    auto fim = unique(s->begin(), s->end(), [](char a, char b) {
-        return a == ' ' && b == ' ';
-    });
-    s->erase(fim, s->end());
-
-    // Remove o espaço caso o último caractere da string seja um espaço
-    if (!s->empty() && s->back() == ' ') {
-        s->pop_back();
-    }
-
-    return *s;
-}
-
-void imprimir(string s[], int tam, int n) {
-    // Se o tamanho da string for menor que o maior tamanho, então ele concatena a diferença de tamanho
-    // em espaços com a string
-    for (int i = 0; i < n; i++) {
-        if (s[i].size() < tam) {
-            s[i] = string(tam - s[i].size(), ' ') + s[i];
+string limparEspacos(string s) {
+    stringstream ss(s); //coleta as palavras de uma string ignorando os espaços antes e depois delas
+    string palavra, resultado = "";
+    while (ss >> palavra) {
+        //atribui a primeira palavra do ss na variável "palavra".
+        if (resultado != "") {
+            resultado += " ";
         }
-
-        cout << s[i] << endl;
+        resultado += palavra;
     }
+    return resultado;
 }
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int n;
+    bool primeiroCaso = true;
 
-    while (cin >> n) {
-        getchar();
+    while (cin >> n && n != 0) {
+        cin.ignore();
 
-        if (n == 0) {
-            return 0;
-        }
-
-        int tam = 0;
-        string s[n];
+        vector<string> linhas(n);
+        int maiorTamanho = 0;
 
         for (int i = 0; i < n; i++) {
-            getline(cin, s[i]);
-        }
-
-        corretor(s);
-
-        for (int i = 0; i < n; i++) {
-            if (s[i].size() > tam) {
-                tam = s[i].size();
+            getline(cin, linhas[i]);
+            linhas[i] = limparEspacos(linhas[i]);
+            if ((int)linhas[i].size() > maiorTamanho) {
+                maiorTamanho = linhas[i].size();
             }
         }
 
-        imprimir(s, tam, n);
+        if (!primeiroCaso) {
+            cout << "\n";
+        }
+        primeiroCaso = false;
+
+        for (int i = 0; i < n; i++) {
+            int espacos = maiorTamanho - linhas[i].size();
+            for (int e = 0; e < espacos; e++) {
+                cout << " ";
+            }
+            cout << linhas[i] << "\n";
+        }
     }
 }
