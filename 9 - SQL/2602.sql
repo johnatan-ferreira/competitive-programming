01af2552a89df8752c9220cfd8fd3d61a9b98333
Johnatan Ferreira <johnatan.fs@protonmail.com>
2026-09-02 12:21:44 -0300

    Update 2602.sql

--- 9 - SQL/2602.sql
+++ 9 - SQL/2602.sql
@@ -7,12 +7,11 @@ CREATE TABLE IF NOT EXISTS customers(
 	credit_limit INT
 );
 
-
 INSERT INTO customers (id, name, street, city, state, credit_limit)
-VALUES (1, 'Pedro Augusto da Rocha', 'Rua Pedro Carlos Hoffman', 'Porto Alegre', 'RS', 700),
-(2, 'Antonio Carlos Mamel', 'Av. Pinheiros', 'Belo Horizonte', 'MG', 3500.50),
-(3, 'Luiza Augusta Mhor', 'Rua Salto Grande', 'Niteroi', 'RJ', 4000.00),
-(4, 'Jane Ester', 'Av 7 de setembro', 'Erechim', 'RS', 800),
-(5, 'Marcos Antônio dos Santos', 'Av Farrapos', 'Porto Alegre', 'RS', 4250.25);
+VALUES 	(1, 'Pedro Augusto da Rocha', 'Rua Pedro Carlos Hoffman', 'Porto Alegre', 'RS', 700),
+		(2, 'Antonio Carlos Mamel', 'Av. Pinheiros', 'Belo Horizonte', 'MG', 3500.50),
+		(3, 'Luiza Augusta Mhor', 'Rua Salto Grande', 'Niteroi', 'RJ', 4000.00),
+		(4, 'Jane Ester', 'Av 7 de setembro', 'Erechim', 'RS', 800),
+		(5, 'Marcos Antônio dos Santos', 'Av Farrapos', 'Porto Alegre', 'RS', 4250.25);
 
 SELECT name FROM customers WHERE state = 'RS';
