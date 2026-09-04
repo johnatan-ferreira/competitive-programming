7253e2d9fe8f82211817705684ae99ebf7dd84fe
Johnatan Ferreira <johnatan.fs@protonmail.com>
2026-09-04 15:48:25 -0300

    Adição do 2618

---
+++ 9 - SQL/2618.sql
@@ -0,0 +1,50 @@
+CREATE TABLE IF NOT EXISTS categories (
+	id INT PRIMARY KEY,
+	name VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS providers (
+	id INT PRIMARY KEY,
+	name VARCHAR(255),
+	street VARCHAR(255),
+	city VARCHAR(255),
+	state CHAR(2)
+);
+
+CREATE TABLE IF NOT EXISTS products (
+	id INT PRIMARY KEY,
+	name VARCHAR(255),
+	amount INT,
+	price REAL,
+	id_categories INT,
+	id_providers INT,
+	FOREIGN KEY (id_categories) REFERENCES categories(id),
+	FOREIGN KEY (id_providers) REFERENCES providers(id)
+);
+
+INSERT INTO categories (id, name)
+VALUES	(1, 'Super Luxury'),
+		(2, 'Imported'),
+		(3, 'Tech'),
+		(4, 'Vintage'),
+		(5, 'Supreme');
+
+INSERT INTO providers (id, name, street, city, state)
+VALUES	(1, 'Ajax SA', 'Rua Presidente Castelo Branco', 'Porto Alegre', 'RS'),
+		(2, 'Sansul SA', 'Av Brasil', 'Rio de Janeiro', 'RJ'),
+		(3, 'South Chairs', 'Rua do Moinho', 'Santa Maria', 'RS'),
+		(4, 'Elon Electro', 'Rua Apolo', 'São Paulo', 'SP'),
+		(5, 'Mike Electro', 'Rua Pedro da Cunha', 'Curitiba', 'PR');
+
+INSERT INTO products (id, name, amount, price, id_providers, id_categories)
+VALUES 	(1, 'Blue Chair', 30, 300.00, 5, 5),
+		(2, 'Red Chair', 50, 2150.00, 2, 1),
+		(3, 'Disney Wardrobe', 400, 829.50, 4, 1),
+		(4, 'Blue Toaster', 20, 9.90, 3, 1),
+		(5, 'TV', 30, 3000.25, 2, 2);
+
+SELECT products.name, providers.name, categories.name
+FROM products
+INNER JOIN categories ON products.id_categories = categories.id
+INNER JOIN providers ON products.id_providers = providers.id
+WHERE categories.name LIKE 'Imported' AND providers.name LIKE 'Sansul SA';
